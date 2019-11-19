6e05409f5b0f85befca21fc4d2af432528d4b681
Sharon Zou <szou00@stuy.edu>
2019-11-19 02:46:01 -0500

    added updates

--- dir.c
+++ dir.c
@@ -8,28 +8,50 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  DIR *direct = opendir(".");
+  //initialization
+  DIR *direct;
   struct dirent *d;
   long size;
 
   struct stat *buf;
   buf = malloc(sizeof(struct stat));
 
-  if (direct == NULL) {
-    printf("Could not open current directory" );
+  char *file = malloc(50);
+
+  //if there is a commnd line argument
+  if (argc>1) {
+    file = argv[1];
+  }
+  //no command line argument. asks user for a directory to scan
+  else {
+    printf("Please enter a directory name: ");
+    fgets(file, 50, stdin);
+    *(strchr(file, '\n')) = '\0'; //get rid of ending
+    printf("\n");
+  }
+
+  // printf("int: %d\n", argc);
+  // printf("directory name: %s\n", argv[1]);
+  // printf("directory: %s\n", file);
+  direct = opendir(file);
+
+  //check for error
+  if (errno || direct == NULL) {
+    printf("Error: %s. Could not open directory. \n", strerror(errno));
     return 0;
   }
 
-  printf("Statistics for directory: .\n");
+  printf("Statistics for directory: %s\n", file);
 
   //size
   while ((d = readdir(direct)) != NULL) {
     stat(d->d_name, buf);
     size += buf->st_size;
   }
-  printf("Total Directory Size: %ld Bytes\n", size);
+  printf("\nTotal Directory Size: %ld Bytes\n\n", size);
+  printf("(Size below considers 1 MB = 1024 Bytes, 1 GB = 1024 MB)\n\n");
 
   char buffer[50];
   sprintf(buffer, "File size: %ld KB\n", size);
@@ -41,10 +63,10 @@ int main() {
     size = size / 1024;
     sprintf(buffer, "File size: %ld GB\n", size);
   }
-  printf("%s", buffer);
+  printf("%s\n", buffer);
 
   //printing directories
-  direct = opendir(".");
+  direct = opendir(file);
   printf("Directories: \n");
   while ((d = readdir(direct)) != NULL) {
     if (d->d_type == 4) {
@@ -53,7 +75,7 @@ int main() {
   }
 
   //printing reg files
-  direct = opendir(".");
+  direct = opendir(file);
   printf("Regular Files: \n");
   while ((d = readdir(direct)) != NULL) {
     if (d->d_type == 8) {
